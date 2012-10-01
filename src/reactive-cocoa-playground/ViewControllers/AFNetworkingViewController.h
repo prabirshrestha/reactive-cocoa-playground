1c1062a212f15510df5e14f4bfe912babbdc76b5
prabirshrestha <mail@prabir.me>
2012-09-30 23:33:09 -0400

    empty AFNetworkingViewController

---
+++ src/reactive-cocoa-playground/ViewControllers/AFNetworkingViewController.h
@@ -0,0 +1,13 @@
+//
+//  AFNetworkingViewController.h
+//  reactive-cocoa-playground
+//
+//  Created by Prabir Shrestha on 9/30/12.
+//  Copyright (c) 2012 Prabir Shrestha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AFNetworkingViewController : UIViewController
+
+@end
