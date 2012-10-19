6815c81d2d70f20eeee76615c0a081ec4ef9b9d9
prabirshrestha <mail@prabir.me>
2012-10-19 19:19:00 -0400

    observable with return

---
+++ src/reactive-cocoa-playground/ViewControllers/CreatingObservablesViewController.h
@@ -0,0 +1,15 @@
+//
+//  CreatingObservablesViewController.h
+//  reactive-cocoa-playground
+//
+//  Created by Prabir Shrestha on 10/19/12.
+//  Copyright (c) 2012 Prabir Shrestha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CreatingObservablesViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UIButton *observableWithReturn;
+
+@end
---
+++ src/reactive-cocoa-playground/ViewControllers/CreatingObservablesViewController.m
@@ -0,0 +1,58 @@
+//
+//  CreatingObservablesViewController.m
+//  reactive-cocoa-playground
+//
+//  Created by Prabir Shrestha on 10/19/12.
+//  Copyright (c) 2012 Prabir Shrestha. All rights reserved.
+//
+
+#import "CreatingObservablesViewController.h"
+#import <ReactiveCocoa.h>
+@interface CreatingObservablesViewController ()
+
+@end
+
+@implementation CreatingObservablesViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    [self setupObservableWithReturn];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (void)viewDidUnload {
+    [self setObservableWithReturn:nil];
+    [super viewDidUnload];
+}
+
+# pragma mark - 
+
+- (void) setupObservableWithReturn {
+    [[self.observableWithReturn rac_subscribableForControlEvents:UIControlEventTouchUpInside]
+     subscribeNext:^(id x) {
+         
+         [[RACSubscribable return:[NSNumber numberWithInt:42]]
+         subscribeNext:^(id x) {
+             NSLog(@"%@", x);
+         }];
+         
+     }];
+}
+
+@end
