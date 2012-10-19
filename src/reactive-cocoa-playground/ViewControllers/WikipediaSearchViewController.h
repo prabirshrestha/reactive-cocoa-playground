ec49249c3f75bcbd032c6e8456ce52b5f6373e29
prabirshrestha <mail@prabir.me>
2012-10-19 19:57:05 -0400

    empty wikipedia serach view contorller

---
+++ src/reactive-cocoa-playground/ViewControllers/WikipediaSearchViewController.h
@@ -0,0 +1,13 @@
+//
+//  WikipediaSearchViewController.h
+//  reactive-cocoa-playground
+//
+//  Created by Prabir Shrestha on 10/19/12.
+//  Copyright (c) 2012 Prabir Shrestha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WikipediaSearchViewController : UIViewController
+
+@end
