2c482d90801c751de529938584dc1b0d0e6964fc
prabirshrestha <mail@prabir.me>
2012-10-21 13:11:26 -0400

    RxOperatorsViewController

---
+++ src/reactive-cocoa-playground/ViewControllers/RxOperatorsViewController.h
@@ -0,0 +1,13 @@
+//
+//  RxOperatorsViewController.h
+//  reactive-cocoa-playground
+//
+//  Created by Prabir Shrestha on 10/21/12.
+//  Copyright (c) 2012 Prabir Shrestha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RxOperatorsViewController : UIViewController
+
+@end
---
+++ src/reactive-cocoa-playground/ViewControllers/RxOperatorsViewController.m
@@ -0,0 +1,38 @@
+//
+//  RxOperatorsViewController.m
+//  reactive-cocoa-playground
+//
+//  Created by Prabir Shrestha on 10/21/12.
+//  Copyright (c) 2012 Prabir Shrestha. All rights reserved.
+//
+
+#import "RxOperatorsViewController.h"
+
+@interface RxOperatorsViewController ()
+
+@end
+
+@implementation RxOperatorsViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
