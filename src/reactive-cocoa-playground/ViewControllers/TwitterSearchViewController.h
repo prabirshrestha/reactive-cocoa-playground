8aa8369240140a0903448c4397d924fa3bd1adb9
prabirshrestha <mail@prabir.me>
2012-11-10 16:03:40 -0500

    TwitterSearchViewController

---
+++ src/reactive-cocoa-playground/ViewControllers/TwitterSearchViewController.h
@@ -0,0 +1,13 @@
+//
+//  TwitterSearchViewController.h
+//  reactive-cocoa-playground
+//
+//  Created by Prabir Shrestha on 11/10/12.
+//  Copyright (c) 2012 Prabir Shrestha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TwitterSearchViewController : UIViewController
+
+@end
---
+++ src/reactive-cocoa-playground/ViewControllers/TwitterSearchViewController.m
@@ -0,0 +1,34 @@
+//
+//  TwitterSearchViewController.m
+//  reactive-cocoa-playground
+//
+//  Created by Prabir Shrestha on 11/10/12.
+//  Copyright (c) 2012 Prabir Shrestha. All rights reserved.
+//
+
+#import "TwitterSearchViewController.h"
+
+@interface TwitterSearchViewController ()
+
+@end
+
+@implementation TwitterSearchViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if(!self) return nil;
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+}
+
+@end
