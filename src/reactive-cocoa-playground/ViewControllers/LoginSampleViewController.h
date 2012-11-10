2bed43a9d25159e07ad2dad994c2853f1286b908
prabirshrestha <mail@prabir.me>
2012-11-09 21:06:27 -0500

    empty Login Sample ViewController

---
+++ src/reactive-cocoa-playground/ViewControllers/LoginSampleViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginSampleViewController.h
+//  reactive-cocoa-playground
+//
+//  Created by Prabir Shrestha on 11/9/12.
+//  Copyright (c) 2012 Prabir Shrestha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginSampleViewController : UIViewController
+
+@end
---
+++ src/reactive-cocoa-playground/ViewControllers/LoginSampleViewController.m
@@ -0,0 +1,38 @@
+//
+//  LoginSampleViewController.m
+//  reactive-cocoa-playground
+//
+//  Created by Prabir Shrestha on 11/9/12.
+//  Copyright (c) 2012 Prabir Shrestha. All rights reserved.
+//
+
+#import "LoginSampleViewController.h"
+
+@interface LoginSampleViewController ()
+
+@end
+
+@implementation LoginSampleViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
