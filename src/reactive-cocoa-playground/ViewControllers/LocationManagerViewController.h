a30105d82e936cd2a2a67589ad15108cade3bf47
prabirshrestha <mail@prabir.me>
2012-11-11 22:19:33 -0500

    added LocationManagerViewController

---
+++ src/reactive-cocoa-playground/ViewControllers/LocationManagerViewController.h
@@ -0,0 +1,13 @@
+//
+//  LocationManagerViewController.h
+//  reactive-cocoa-playground
+//
+//  Created by Prabir Shrestha on 11/11/12.
+//  Copyright (c) 2012 Prabir Shrestha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LocationManagerViewController : UIViewController
+
+@end
---
+++ src/reactive-cocoa-playground/ViewControllers/LocationManagerViewController.m
@@ -0,0 +1,38 @@
+//
+//  LocationManagerViewController.m
+//  reactive-cocoa-playground
+//
+//  Created by Prabir Shrestha on 11/11/12.
+//  Copyright (c) 2012 Prabir Shrestha. All rights reserved.
+//
+
+#import "LocationManagerViewController.h"
+
+@interface LocationManagerViewController ()
+
+@end
+
+@implementation LocationManagerViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
