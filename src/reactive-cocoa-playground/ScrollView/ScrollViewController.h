74c562c09c6153c1d2a5942352e6eabcb85323d1
Ash Furrow <ash@ashfurrow.com>
2013-04-05 16:41:39 -0400

    Added basic scroll view example, which illustrates binding values from a UIPageControl to/from a UIScrollView.

---
+++ src/reactive-cocoa-playground/ScrollView/ScrollViewController.h
@@ -0,0 +1,13 @@
+//
+//  ScrollViewController.h
+//  reactive-cocoa-playground
+//
+//  Created by Ash Furrow on 2013-04-05.
+//  Copyright (c) 2013 Prabir Shrestha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ScrollViewController : UIViewController
+
+@end
